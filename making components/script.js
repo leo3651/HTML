4e74dbf1650e82aa030dee46f2c1141df13eed2e
Leo <leo.kowacevic@gmail.com>
2024-01-09 13:19:21 +0100

    adding js

--- making components/script.js
+++ making components/script.js
@@ -1,1 +1,7 @@
-document.querySelector();
+const icons = document.querySelectorAll(".icon");
+for (let i = 0, n = icons.length; i < n; i++) {
+  icons[i].addEventListener("click", (event) => {
+    const accordion = document.querySelectorAll(".accordion");
+    accordion[i].classList.toggle("open");
+  });
+}
